c707d374efb7379db44b9fe3d6e256b95d5f6b9e
Cho Hyun Jun <ed.hj.cho@gmail.com>
2025-02-18 14:08:16 +0900

    wordcloud update

--- wordcloud.py
+++ wordcloud.py
@@ -1,57 +1,19 @@
-아미,19
-메종키츠네,17
-겐조,9
-세인트제임스,9
-막스마라,23
-몽클레르,16
-스톤아일랜드,7
-까르띠에,5
-구찌,31
-발렌시아가,15
-페라가모,26
-아페쎄,20
-샤넬,16
-나이키,40
-아디다스,34
-레노마,38
-조말론,13
-오메가,8
-설화수,17
-SJSJ,19
-바쉬,16
-베즐리,16
-문화센터,16
-라운지,10
-주차,43
-발렛,20
-교환,50
-환불,44
-불친절,36
-칭찬,10
-식품관,22
-푸드코트,16
-이벤트,34
-팝업,18
-유아,14
-유모차,8
-상품권,30
-자스민,12
-세이지,8
-그린,8
-할인,50
-마일리지,28
-포인트,16
-휴점,10
-선물세트,18
-배송,26
-취소,30
-무이자,36
-할부,28
-쿠폰,28
-한도,8
-발급,14
-재발급,8
-포인트워크,8
-마케팅동의,12
-포인트,30
-클럽웨딩,16
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv("wordcloud_3.csv",encoding="utf-8")  #워드클라우드로 만들기 위한 데이터
+wc = df.set_index("title").to_dict()["count"]
+
+wordCloud = WordCloud(
+    font_path='/usr/share/fonts/truetype/nanum/NanumGothicBold.ttf',  #google colabs 에서 하기 위함
+    width = 400,
+    height = 400,
+    colormap = 'viridis',
+    max_font_size=80,
+    background_color='white'
+).generate_from_frequencies(wc)
+
+plt.figure()
+plt.imshow(wordCloud)
+plt.axis('off')
